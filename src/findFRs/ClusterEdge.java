dd061200fff72e13954712aa694dabe9293aef63
bmumey <brendan.mumey@montana.edu>
2017-07-27 18:21:46 -0600

    bug fix in clusterEdge.other

--- src/findFRs/ClusterEdge.java
+++ src/findFRs/ClusterEdge.java
@@ -13,15 +13,15 @@ public class ClusterEdge implements Comparable<ClusterEdge> {
 
     ClusterNode u, v;
     int potentialSup;
-    
+
     ClusterEdge(ClusterNode u, ClusterNode v, int sup) {
         this.u = u;
         this.v = v;
         potentialSup = sup;
     }
-    
-    ClusterNode other(ClusterNode u) {
-        if (this.u == u) {
+
+    ClusterNode other(ClusterNode x) {
+        if (u == x) {
             return v;
         } else {
             return u;
@@ -31,7 +31,7 @@ public class ClusterEdge implements Comparable<ClusterEdge> {
     public int compareTo(ClusterEdge other) {
         int result = Integer.compare(other.potentialSup, potentialSup);
         if (result == 0) {
-            result = Integer.compare(Math.abs(u.size-v.size), Math.abs(other.u.size - other.v.size));
+            result = Integer.compare(Math.abs(u.size - v.size), Math.abs(other.u.size - other.v.size));
         }
         if (result == 0) {
             result = u.compareTo(other.u);
